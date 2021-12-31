443b50fce04e703743582a402b70a6a4305902b2
Vanessasaurus <814322+vsoch@users.noreply.github.com>
2021-12-30 23:04:34 -0700

    test paks containers (#6)
    * test paks containers
    Signed-off-by: vsoch <vsoch@users.noreply.github.com>

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,36 +2,33 @@
 
 # These envars are required!
 
-if [ -z "${INPUT_TOKEN}" ]; then
+if [ -z "${token}" ]; then
     printf "You are required to provide a GitHub token.\n"
     exit 1
 fi
 
 # Ensure input token is defined
-export GITHUB_TOKEN=${INPUT_TOKEN}
+export GITHUB_TOKEN=${token}
 
-if [ -z "${INPUT_USER}" ]; then
+if [ -z "${user}" ]; then
     printf "Please provide a user input for the username associated with the token.\n"
     exit 1
 fi
 
-if [ -z "${INPUT_PACKAGE}" ]; then
+if [ -z "${package}" ]; then
     printf "An input package or spec string is required to build.\n"
     exit 1
 fi
 
 # If deploy is set to true and we don't have a uri, this is an error 
-if [ "${INPUT_DEPLOY}" == "true" ] && [ -z "${INPUT_URI}" ]; then
+if [ "${deploy}" == "true" ] && [ -z "${uri}" ]; then
     printf "If you want to deploy you must define a uri (unique resource identifier)\n"
     exit 1
 fi
-# Repository name
-repository_name=$(basename ${PWD})
-printf "Repository name is ${repository_name}\n"
 
 # Run paks for the specs provided, with deploy or not
-if [ "${INPUT_DEPLOY}" == "true" ]; then
-    paks build "${INPUT_PACKAGE}"
+if [ "${deploy}" == "true" ]; then
+    paks build "${package}"
 else
-    paks build "${INPUT_PACKAGE}" --push "${INPUT_URI}"
+    paks build "${package}" --push "${uri}"
 fi
